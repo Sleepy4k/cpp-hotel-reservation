a9430b6ba8f282c76101f89ea49b9386055b6261
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-27 17:43:11 +0700

    chore(model): add crud for user model

--- src/model/user.hpp
+++ src/model/user.hpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include "../const/path.hpp"
+#include "../handler/hash.hpp"
 #include "../storage/file.hpp"
 
 using namespace std;
@@ -13,206 +14,348 @@ using namespace std;
  * @struct User
  */
 struct User {
-  string uid;
-  string role;
-  string username;
-  string password;
+  private:
+    string uid;
+    string role;
+    string username;
+    string password;
 
-  /**
-   * @brief Construct a new User object
-   * 
-   * @param uid 
-   * @param role 
-   * @param username 
-   * @param password 
-   */
-  User(string uid, string role, string username, string password) {
-    this->uid = uid;
-    this->role = role;
-    this->username = username;
-    this->password = password;
-  }
+  public:
 
-  /**
-   * @brief Construct a new User object
-   */
-  User() {
-    this->uid = "";
-    this->role = "";
-    this->username = "";
-    this->password = "";
-  }
-
-  /**
-   * @brief Get the user object
-   * 
-   * @param username string
-   * @param password string
-   * 
-   * @return User
-   */
-  User get_user(string username, string password) {
-    string content = File::read(Path::getPath() + "/user.csv");
-
-    User user;
-    string line;
-    vector<string> row;
-    stringstream ss(content);
-
-    while (getline(ss, line, '\n')) {
-      string word;
-      stringstream s(line);
-
-      while (getline(s, word, ',')) {
-        row.push_back(word);
-      }
-
-      if (row[2] == username) {
-        user = User(row[0], row[1], row[2], row[3]);
-      }
-
-      row.clear();
+    /**
+     * @brief Construct a new User object
+     * 
+     * @param uid 
+     * @param role 
+     * @param username 
+     * @param password 
+     */
+    User(string uid, string role, string username, string password) {
+      this->uid = uid;
+      this->role = role;
+      this->username = username;
+      this->password = password;
     }
 
-    return user;
-  }
-
-  /**
-   * @brief Create the user
-   * 
-   * @return void
-   */
-  void create() {
-    string content = File::read(Path::getPath() + "/user.csv");
-
-    string line;
-    vector<string> row;
-    bool is_exist = false;
-    stringstream ss(content);
-
-    while (getline(ss, line, '\n')) {
-      string word;
-      stringstream s(line);
-
-      while (getline(s, word, ',')) {
-        row.push_back(word);
-      }
-
-      if (row[2] == this->username) {
-        is_exist = true;
-      }
-
-      row.clear();
+    /**
+     * @brief Construct a new User object
+     */
+    User() {
+      this->uid = "";
+      this->role = "";
+      this->username = "";
+      this->password = "";
     }
 
-    if (is_exist) {
-      cout << "Username already exist" << endl;
-      return;
-    } else {
-      fstream outfile(Path::getPath() + "/user.csv", ios::app);
+    /**
+     * @brief Set the uid object
+     * 
+     * @param uid string
+     * 
+     * @return void
+     */
+    void set_uid(string uid) {
+      this->uid = uid;
+    }
 
-      if (!outfile) {
-        outfile.close();
-        cerr << "Error opening file" << endl;
+    /**
+     * @brief Set the role object
+     * 
+     * @param role string
+     * 
+     * @return void
+     */
+    void set_role(string role) {
+      this->role = role;
+    }
+
+    /**
+     * @brief Set the username object
+     * 
+     * @param username string
+     * 
+     * @return void
+     */
+    void set_username(string username) {
+      this->username = username;
+    }
+
+    /**
+     * @brief Set the password object
+     * 
+     * @param password string
+     * 
+     * @return void
+     */
+    void set_password(string password) {
+      this->password = password;
+    }
+
+    /**
+     * @brief Get the uid object
+     * 
+     * @return string 
+     */
+    string get_uid() {
+      return this->uid;
+    }
+
+    /**
+     * @brief Get the role object
+     * 
+     * @return string 
+     */
+    string get_role() {
+      return this->role;
+    }
+
+    /**
+     * @brief Get the username object
+     * 
+     * @return string 
+     */
+    string get_username() {
+      return this->username;
+    }
+
+    /**
+     * @brief Get the password object
+     * 
+     * @return string 
+     */
+    string get_password() {
+      return this->password;
+    }
+
+    /**
+     * @brief Get all the user
+     * 
+     * @return vector<string> 
+     */
+    vector<User> get() {
+      string content = File::read(Path::getPath() + "/user.csv");
+
+      string line;
+      vector<User> data;
+      vector<string> row;
+      stringstream ss(content);
+
+      while (getline(ss, line, '\n')) {
+        string word;
+        stringstream s(line);
+
+        while (getline(s, word, ',')) {
+          row.push_back(word);
+        }
+
+        data.push_back(User(row[0], row[1], row[2], row[3]));
+
+        row.clear();
+      }
+
+      return data;
+    }
+
+    /**
+     * @brief Create the user
+     * 
+     * @return void
+     */
+    void create(bool allow_print = true) {
+      string content = File::read(Path::getPath() + "/user.csv");
+
+      string line;
+      vector<string> row;
+      bool is_exist = false;
+      stringstream ss(content);
+
+      while (getline(ss, line, '\n')) {
+        string word;
+        stringstream s(line);
+
+        while (getline(s, word, ',')) {
+          row.push_back(word);
+        }
+
+        if (row[2] == this->username) {
+          is_exist = true;
+        }
+
+        row.clear();
+      }
+
+      if (is_exist) {
+        cout << "Username already exist" << endl;
+        return;
       } else {
-        outfile << this->uid << "," << this->role << "," << this->username << "," << this->password << endl;
-        outfile.close();
-        cout << "User created successfully" << endl;
+        fstream outfile(Path::getPath() + "/user.csv", ios::app);
+
+        if (!outfile) {
+          outfile.close();
+          cerr << "Error opening file" << endl;
+        } else {
+          outfile << this->uid << "," << this->role << "," << this->username << "," << this->password << endl;
+          outfile.close();
+
+          if (allow_print) {
+            cout << "User created successfully" << endl;
+          }
+        }
       }
     }
-  }
 
-  /**
-   * @brief Set the uid object
-   * 
-   * @param uid string
-   * 
-   * @return void
-   */
-  void set_uid(string uid) {
-    this->uid = uid;
-  }
+    /**
+     * @brief Get the user object
+     * 
+     * @param username string
+     * 
+     * @return User
+     */
+    User find(string username) {
+      string content = File::read(Path::getPath() + "/user.csv");
 
-  /**
-   * @brief Set the role object
-   * 
-   * @param role string
-   * 
-   * @return void
-   */
-  void set_role(string role) {
-    this->role = role;
-  }
+      User user;
+      string line;
+      vector<string> row;
+      stringstream ss(content);
 
-  /**
-   * @brief Set the username object
-   * 
-   * @param username string
-   * 
-   * @return void
-   */
-  void set_username(string username) {
-    this->username = username;
-  }
+      while (getline(ss, line, '\n')) {
+        string word;
+        stringstream s(line);
 
-  /**
-   * @brief Set the password object
-   * 
-   * @param password string
-   * 
-   * @return void
-   */
-  void set_password(string password) {
-    this->password = password;
-  }
+        while (getline(s, word, ',')) {
+          row.push_back(word);
+        }
 
-  /**
-   * @brief Get the uid object
-   * 
-   * @return string 
-   */
-  string get_uid() {
-    return this->uid;
-  }
+        if (row[2] == username) {
+          user = User(row[0], row[1], row[2], row[3]);
+        }
 
-  /**
-   * @brief Get the role object
-   * 
-   * @return string 
-   */
-  string get_role() {
-    return this->role;
-  }
+        row.clear();
+      }
 
-  /**
-   * @brief Get the username object
-   * 
-   * @return string 
-   */
-  string get_username() {
-    return this->username;
-  }
+      return user;
+    }
 
-  /**
-   * @brief Get the password object
-   * 
-   * @return string 
-   */
-  string get_password() {
-    return this->password;
-  }
+    /**
+     * @brief Update the user
+     * 
+     * @return void
+     */
+    void update(string username, string old_password, string new_password) {
+      string content = File::read(Path::getPath() + "/user.csv");
 
-  /**
-   * @brief Get the string object
-   * 
-   * @return string 
-   */
-  string toString() {
-    return "UID: " + this->uid + "\n"
-           + "Role: " + this->role + "\n"
-           + "Username: " + this->username + "\n"
-           + "Password: " + this->password;
-  }
+      string line;
+      vector<string> row;
+      bool is_exist = false;
+      stringstream ss(content);
+
+      while (getline(ss, line, '\n')) {
+        string word;
+        stringstream s(line);
+
+        while (getline(s, word, ',')) {
+          row.push_back(word);
+        }
+
+        if (row[2] == username && row[3] == old_password) {
+          is_exist = true;
+        }
+
+        row.clear();
+      }
+
+      if (is_exist) {
+        vector<User> users = this->get();
+
+        fstream outfile(Path::getPath() + "/user.csv", ios::out | ios::trunc);
+
+        if (!outfile) {
+          outfile.close();
+          cerr << "Error opening file" << endl;
+        } else {
+          outfile << "";
+          outfile.close();
+
+          for (int i = 0; i < users.size(); i++) {
+            if (users[i].get_username() == username) {
+              users[i].set_password(new_password);
+            }
+
+            users[i].create(false);
+          }
+
+          cout << "User updated successfully" << endl;
+        }
+      } else {
+        cout << "User not found" << endl;
+      }
+    }
+
+    /**
+     * @brief Delete the user
+     * 
+     * @return void
+     */
+    void delete_user(string username, string password) {
+      string content = File::read(Path::getPath() + "/user.csv");
+
+      string line;
+      vector<string> row;
+      bool is_exist = false;
+      stringstream ss(content);
+
+      while (getline(ss, line, '\n')) {
+        string word;
+        stringstream s(line);
+
+        while (getline(s, word, ',')) {
+          row.push_back(word);
+        }
+
+        if (row[2] == username && row[3] == password) {
+          is_exist = true;
+        }
+
+        row.clear();
+      }
+
+      if (is_exist) {
+        vector<User> users = this->get();
+
+        fstream outfile(Path::getPath() + "/user.csv", ios::out | ios::trunc);
+
+        if (!outfile) {
+          outfile.close();
+          cerr << "Error opening file" << endl;
+        } else {
+          outfile << "";
+          outfile.close();
+
+          for (int i = 0; i < users.size(); i++) {
+            if (users[i].get_username() != username) {
+              users[i].create(false);
+            }
+          }
+
+          cout << "User deleted successfully" << endl;
+        }
+      } else {
+        cout << "User not found" << endl;
+      }
+    }
+
+    /**
+     * @brief Get the string object
+     * 
+     * @return string 
+     */
+    string toString() {
+      return "UID: " + this->uid + "\n"
+            + "Role: " + this->role + "\n"
+            + "Username: " + this->username + "\n"
+            + "Password: " + this->password;
+    }
 };
 
 
