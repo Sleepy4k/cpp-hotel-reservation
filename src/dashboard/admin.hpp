ed98bc99d3fe782fcb693a17ff00a9e0dfd634d1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-28 20:08:45 +0700

    chore(dashboard): add code documentation

--- src/dashboard/admin.hpp
+++ src/dashboard/admin.hpp
@@ -7,8 +7,20 @@ using namespace std;
 #ifndef DASHBOARD_ADMIN_HPP
 #define DASHBOARD_ADMIN_HPP
 
+/**
+ * @brief AdminDashboard class to handle admin dashboard
+ * 
+ * @class AdminDashboard
+ */
 class AdminDashboard {
   public:
+    /**
+     * @brief Show the admin dashboard
+     * 
+     * @param user User
+     * 
+     * @return void
+     */
     static void dashboard(User user) {
       cout << "Admin Dashboard" << endl;
       cout << "Welcome " << user.get_username() << endl;
--- src/dashboard/user.hpp
+++ src/dashboard/user.hpp
@@ -7,8 +7,20 @@ using namespace std;
 #ifndef DASHBOARD_USER_HPP
 #define DASHBOARD_USER_HPP
 
+/**
+ * @brief UserDashboard class to handle user dashboard
+ * 
+ * @class UserDashboard
+ */
 class UserDashboard {
   public:
+    /**
+     * @brief Show the user dashboard
+     * 
+     * @param user User
+     * 
+     * @return void
+     */
     static void dashboard(User user) {
       cout << "User Dashboard" << endl;
       cout << "Welcome " << user.get_username() << endl;
