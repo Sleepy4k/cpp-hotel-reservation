7305c6008a2b011b3e365405d218bc00e374be14
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-30 15:31:19 +0700

    feat(enum): language enum

---
+++ src/enum/language.hpp
@@ -0,0 +1,14 @@
+#ifndef ENUM_LANGUAGE_HPP
+#define ENUM_LANGUAGE_HPP
+
+/**
+ * @brief Language enum class
+ * 
+ * @enum Language
+ */
+enum class Language {
+  ENGLISH,
+  INDONESIA
+};
+
+#endif
