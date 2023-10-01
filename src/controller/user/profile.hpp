2e0c75f9c8043c17e6b8de2ca294b8f391f1c685
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-01 19:57:18 +0700

    feat(controller): add user profile

---
+++ src/controller/user/profile.hpp
@@ -0,0 +1,52 @@
+#include "../../const/app.hpp"
+#include "../../model/user.hpp"
+#include "../../auth/forgot.hpp"
+#include "../../const/menu_list.hpp"
+#include "../../handler/validation.hpp"
+
+using namespace std;
+
+#ifndef CONTROLLER_USER_PROFILE_HPP
+#define CONTROLLER_USER_PROFILE_HPP
+
+/**
+ * @brief UserProfileDashboard class to handle user profile dashboard
+ * 
+ * @class UserProfileDashboard
+ */
+class UserProfileDashboard {
+  public:
+    /**
+     * @brief Dashboard function to show user profile dashboard
+     * 
+     * @param user 
+     */
+    static void dashboard(User user) {
+      vector<string> menu_list = MenuList::USER_PROFILE_DASHBOARD_MENU;
+
+      cout << "\n\n--------------------------------------------------------------------" << endl;
+      cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
+      cout << "--------------------------------------------------------------------" << endl;
+      cout << Sprintf::format(get_translated_string("menu_list_user_profile_title"), {user.get_username()}) << endl;
+    
+      for (int i = 0; i < menu_list.size(); i++) {
+        cout << get_translated_string("menu_list_user_profile_press") << " " << i + 1 << " --> " << get_translated_string(menu_list[i]) << endl;
+      }
+
+      int menu = Validation::integer_validation(1, menu_list.size());
+
+      switch (menu) {
+        case 1:
+          Forgot::validate_forgot_question(user);
+          break;
+        case 2:
+          return;
+          break;
+        default:
+          cout << get_translated_string("menu_list_user_profile_invalid") << endl;
+          break;
+      }
+    };
+};
+
+#endif
