8c855e6aaedef80ec2e90416f9c0cc1cdd885d0d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-06 11:30:49 +0700

    chore(dashboard): add loop to main dashboard

--- src/dashboard/admin.hpp
+++ src/dashboard/admin.hpp
@@ -3,6 +3,7 @@
 #ifndef DASHBOARD_ADMIN_HPP
 #define DASHBOARD_ADMIN_HPP
 
+#include "../controller/admin/room.hpp"
 #include "../controller/admin/system.hpp"
 
 /**
@@ -22,45 +23,45 @@ class AdminDashboard {
     static void dashboard(User user) {
       vector<string> menu_list = MenuList::ADMIN_DASHBOARD_MENU;
 
-      cout << "\n\n--------------------------------------------------------------------" << endl;
-      cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
-      cout << "--------------------------------------------------------------------" << endl;
-      cout << Sprintf::format(get_translated_string("menu_list_admin_title"), {user.get_username()}) << endl;
+      while (true) {
+        cout << "\n\n--------------------------------------------------------------------" << endl;
+        cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
+        cout << "--------------------------------------------------------------------" << endl;
+        cout << Sprintf::format(get_translated_string("menu_list_admin_title"), {user.get_username()}) << endl;
 
-      for (unsigned int i = 0; i < menu_list.size(); i++) {
-        cout << get_translated_string("menu_list_admin_press") << " " << i + 1 << " --> " << get_translated_string(menu_list[i]) << '\n';
+        for (unsigned int i = 0; i < menu_list.size(); i++) {
+          cout << get_translated_string("menu_list_admin_press") << " " << i + 1 << " --> " << get_translated_string(menu_list[i]) << '\n';
+        }
+
+        int menu = Validation::integer_validation(1, menu_list.size());
+
+        switch (menu) {
+          case 1:
+            AdminRoomDashboard::dashboard(user);
+            break;
+          case 2:
+            cout << get_translated_string("menu_list_admin_manage_user") << endl;
+            break;
+          case 3:
+            cout << get_translated_string("menu_list_admin_manage_booking") << endl;
+            break;
+          case 4:
+            cout << get_translated_string("menu_list_admin_manage_admin") << endl;
+            break;
+          case 5:
+            cout << get_translated_string("menu_list_admin_report_and_statistic") << endl;
+            break;
+          case 6:
+            AdminSystemDashboard::dashboard(user);
+            break;
+          case 7:
+            return;
+            break;
+          default:
+            cout << get_translated_string("menu_list_admin_invalid") << endl;
+            break;
+        }
       }
-
-      int menu = Validation::integer_validation(1, menu_list.size());
-
-      switch (menu) {
-        case 1:
-          cout << get_translated_string("menu_list_admin_manage_room") << endl;
-          break;
-        case 2:
-          cout << get_translated_string("menu_list_admin_manage_user") << endl;
-          break;
-        case 3:
-          cout << get_translated_string("menu_list_admin_manage_booking") << endl;
-          break;
-        case 4:
-          cout << get_translated_string("menu_list_admin_manage_admin") << endl;
-          break;
-        case 5:
-          cout << get_translated_string("menu_list_admin_report_and_statistic") << endl;
-          break;
-        case 6:
-          AdminSystemDashboard::dashboard(user);
-          break;
-        case 7:
-          return;
-          break;
-        default:
-          cout << get_translated_string("menu_list_admin_invalid") << endl;
-          break;
-      }
-
-      dashboard(user);
     };
 };
 
--- src/dashboard/user.hpp
+++ src/dashboard/user.hpp
@@ -22,39 +22,39 @@ class UserDashboard {
     static void dashboard(User user) {
       vector<string> menu_list = MenuList::USER_DASHBOARD_MENU;
 
-      cout << "\n\n--------------------------------------------------------------------" << endl;
-      cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
-      cout << "--------------------------------------------------------------------" << endl;
-      cout << Sprintf::format(get_translated_string("menu_list_user_title"), {user.get_username()}) << endl;
+      while (true) {
+        cout << "\n\n--------------------------------------------------------------------" << endl;
+        cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
+        cout << "--------------------------------------------------------------------" << endl;
+        cout << Sprintf::format(get_translated_string("menu_list_user_title"), {user.get_username()}) << endl;
 
-      for (unsigned int i = 0; i < menu_list.size(); i++) {
-        cout << get_translated_string("menu_list_user_press") << " " << i + 1 << " --> " << get_translated_string(menu_list[i]) << '\n';
+        for (unsigned int i = 0; i < menu_list.size(); i++) {
+          cout << get_translated_string("menu_list_user_press") << " " << i + 1 << " --> " << get_translated_string(menu_list[i]) << '\n';
+        }
+
+        int menu = Validation::integer_validation(1, menu_list.size());
+
+        switch (menu) {
+          case 1:
+            cout << get_translated_string("menu_list_user_search_room") << endl;
+            break;
+          case 2:
+            cout << get_translated_string("menu_list_user_booking_room") << endl;
+            break;
+          case 3:
+            cout << get_translated_string("menu_list_user_booking_history") << endl;
+            break;
+          case 4:
+            UserProfileDashboard::dashboard(user);
+            break;
+          case 5:
+            return;
+            break;
+          default:
+            cout << get_translated_string("menu_list_user_invalid") << endl;
+            break;
+        }
       }
-
-      int menu = Validation::integer_validation(1, menu_list.size());
-
-      switch (menu) {
-        case 1:
-          cout << get_translated_string("menu_list_user_search_room") << endl;
-          break;
-        case 2:
-          cout << get_translated_string("menu_list_user_booking_room") << endl;
-          break;
-        case 3:
-          cout << get_translated_string("menu_list_user_booking_history") << endl;
-          break;
-        case 4:
-          UserProfileDashboard::dashboard(user);
-          break;
-        case 5:
-          return;
-          break;
-        default:
-          cout << get_translated_string("menu_list_user_invalid") << endl;
-          break;
-      }
-
-      dashboard(user);
     };
 };
 
