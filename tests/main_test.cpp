6547d4b5c804af66e284f3cf9711a3166e55cef7
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-06 11:31:29 +0700

    chore(test): add helper for main test

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -16,11 +16,25 @@
 int main(int argc, char* argv[]) {
   Path::setPath(argv[0]);
 
+  cout << "Running hash tests..." << endl;
   run_hash_test();
+  cout << "Hash tests passed!" << endl;
+
+  cout << "Running validation tests..." << endl;
   run_validation_test();
+  cout << "Validation tests passed!" << endl;
+
+  cout << "Running sprintf tests..." << endl;
   run_sprintf_test();
+  cout << "Sprintf tests passed!" << endl;
+
+  cout << "Running uuid tests..." << endl;
   run_uuid_test();
+  cout << "Uuid tests passed!" << endl;
+
+  cout << "Running file tests..." << endl;
   run_file_test();
+  cout << "File tests passed!" << endl;
 
   cout << "All tests passed!" << endl;
   return 0;
