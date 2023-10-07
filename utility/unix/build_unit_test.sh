c182c016380fb1582776e224428ff08116b5a37d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-07 18:29:04 +0700

    feat(utility): add unix shell script

---
+++ utility/unix/build_unit_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to compile and run a C++ program with g++
+echo "Compiling C++ program with g++"
+
+# Source file name (change this to your C++ source file)
+source_file="../../tests/main_test.cpp"
+
+# Output binary name (change this to your desired output name)
+output_binary="../../output/main_test"
+
+# Compile the C++ program
+g++ -std=c++11 -o "$output_binary" "$source_file"
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+  # Run the compiled binary
+  echo "Compilation successful. Running $output_binary..."
+  "./output/$output_binary"
+else
+  echo "Compilation failed."
+fi
