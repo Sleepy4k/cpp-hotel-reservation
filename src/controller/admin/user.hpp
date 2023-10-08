638e0bb8e4b084ae1af7bbf5441f40303146ed0d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-08 18:24:25 +0700

    chore(controller): refactor code in user admin

--- src/controller/admin/user.hpp
+++ src/controller/admin/user.hpp
@@ -13,6 +13,8 @@ class AdminUserDashboard {
     /**
      * @brief List all users
      * 
+     * @param user User
+     * 
      * @return void
     */
     static void list(User user) {
@@ -21,26 +23,29 @@ class AdminUserDashboard {
       cout << "\n\n--------------------------------------------------------------------" << endl;
       cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
       cout << "--------------------------------------------------------------------" << endl;
-      cout << get_translated_string("menu_list_admin_user_list") << endl;
+      cout << get_translated_string("admin_user_list_title") << endl;
 
       int totalUsers = users.size();
 
       if (totalUsers > 0) {
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_title"), {to_string(totalUsers)}) << endl;
+        int totalAdmin = 0;
 
         for (unsigned int i = 0; i < totalUsers; i++) {
           if (users[i].get_role() == "admin") {
+            totalAdmin++;
             continue;
           }
 
           cout << "\n--------------------------------------------------------" << '\n';
-          cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_uid"), {users[i].get_uid()}) << '\n';
-          cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_username"), {users[i].get_username()}) << '\n';
-          cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_role"), {users[i].get_role()}) << '\n';
+          cout << Sprintf::format(get_translated_string("admin_user_list_data_uid"), {users[i].get_uid()}) << '\n';
+          cout << Sprintf::format(get_translated_string("admin_user_list_data_role"), {users[i].get_role()}) << '\n';
+          cout << Sprintf::format(get_translated_string("admin_user_list_data_username"), {users[i].get_username()}) << '\n';
           cout << "--------------------------------------------------------" << '\n';
         }
+
+        cout << '\n' << Sprintf::format(get_translated_string("admin_user_list_header"), {to_string(totalUsers - totalAdmin)}) << endl;
       } else {
-        cout << get_translated_string("menu_list_admin_user_list_empty") << endl;
+        cout << get_translated_string("admin_user_list_empty") << endl;
       }
 
       Getky::pause();
@@ -49,84 +54,88 @@ class AdminUserDashboard {
     /**
      * @brief Search user by username
      * 
+     * @param user User
+     * 
      * @return void
     */
     static void search(User user) {
       cout << "\n\n--------------------------------------------------------------------" << endl;
       cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
       cout << "--------------------------------------------------------------------" << endl;
-      cout << get_translated_string("menu_list_admin_user_search") << endl;
+      cout << get_translated_string("admin_user_search_title") << endl;
 
-      string username = Validation::string_validation(get_translated_string("menu_list_admin_user_search_input_username"), "username");
-
+      string username = Validation::string_validation(get_translated_string("admin_user_input_username"), "username");
       User data = user.find(username);
 
-      if (data.get_uid() != "" && data.get_role() != "admin") {
-        cout << "\n--------------------------------------------------------" << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_uid"), {data.get_uid()}) << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_username"), {data.get_username()}) << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_role"), {data.get_role()}) << '\n';
-        cout << "--------------------------------------------------------" << '\n';
-      } else {
-        cout << get_translated_string("menu_list_admin_user_search_not_found") << endl;
+      if (data.get_uid() == "" || data.get_role() == "admin") {
+        cout << Sprintf::format(get_translated_string("admin_user_not_found"), {username}) << endl;
+        return;
       }
+
+      cout << "\n--------------------------------------------------------" << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_uid"), {data.get_uid()}) << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_role"), {data.get_role()}) << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_username"), {data.get_username()}) << '\n';
+      cout << "--------------------------------------------------------" << '\n';
+
+      Getky::pause();
     }
 
     /**
      * @brief Edit user by username
      * 
+     * @param user User
+     * 
      * @return void
     */
-    static void edit() {
+    static void edit(User user) {
       cout << "\n\n--------------------------------------------------------------------" << endl;
       cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
       cout << "--------------------------------------------------------------------" << endl;
-      cout << get_translated_string("menu_list_admin_user_edit") << endl;
+      cout << get_translated_string("admin_user_edit_title") << endl;
 
-      string username = Validation::string_validation(get_translated_string("menu_list_admin_user_edit_input_username"), "username");
-
-      User user;
+      string username = Validation::string_validation(get_translated_string("admin_user_input_username"), "username");
       User data = user.find(username);
 
-      if (data.get_uid() != "" && data.get_role() != "admin") {
-        cout << "\n--------------------------------------------------------" << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_uid"), {data.get_uid()}) << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_username"), {data.get_username()}) << '\n';
-        cout << Sprintf::format(get_translated_string("menu_list_admin_user_list_data_role"), {data.get_role()}) << '\n';
-        cout << "--------------------------------------------------------" << '\n';
-      } else {
-        cout << get_translated_string("menu_list_admin_user_edit_not_found") << endl;
+      if (data.get_uid() == "" || data.get_role() == "admin") {
+        cout << Sprintf::format(get_translated_string("admin_user_not_found"), {username}) << endl;
         return;
       }
 
-      string role = Validation::string_validation(get_translated_string("menu_list_admin_user_edit_input_role"), "role");
+      cout << "\n--------------------------------------------------------" << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_uid"), {data.get_uid()}) << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_role"), {data.get_role()}) << '\n';
+      cout << Sprintf::format(get_translated_string("admin_user_list_data_username"), {data.get_username()}) << '\n';
+      cout << "--------------------------------------------------------" << '\n';
 
-      if (role == "admin" || role == "user") {
+      string role = Validation::string_validation(get_translated_string("admin_user_input_role"), "role");
+
+      if (role == "admin" || role == "user" || role == "pengguna") {
+        (role == "pengguna") ? role = "user" : role = role;
         user.update_role(username, role);
-        cout << get_translated_string("menu_list_admin_user_edit_success") << endl;
       } else {
-        cout << get_translated_string("menu_list_admin_user_edit_invalid") << endl;
+        cout << get_translated_string("admin_user_edit_invalid") << endl;
       }
     }
 
     /**
      * @brief Delete user by username
      * 
+     * @param user User
+     * 
      * @return void
     */
-    static void destroy() {
+    static void destroy(User user) {
       cout << "\n\n--------------------------------------------------------------------" << endl;
       cout << "               " << get_translated_string(App::APP_NAME) << "                  " << endl;
       cout << "--------------------------------------------------------------------" << endl;
-      cout << get_translated_string("menu_list_admin_user_delete") << endl;
+      cout << get_translated_string("admin_user_delete_title") << endl;
 
-      string username = Validation::string_validation(get_translated_string("menu_list_admin_user_delete_input_username"), "username");
-
-      User user;
+      string username = Validation::string_validation(get_translated_string("admin_user_input_username"), "username");
       User data = user.find(username);
 
       if (data.get_role() == "admin") {
-        cout << get_translated_string("menu_list_admin_user_delete_not_found") << endl;
+        cout << Sprintf::format(get_translated_string("admin_user_not_found"), {username}) << endl;
         return;
       }
 
@@ -156,26 +165,19 @@ class AdminUserDashboard {
         int menu = Validation::integer_validation(1, menu_list.size());
 
         switch (menu) {
-          case 1:
-            list(user);
+          case 1: list(user);
             break;
-          case 2:
-            Register::register_user("user");
+          case 2: Register::register_user("user", false);
             break;
-          case 3:
-            search(user);
+          case 3: search(user);
             break;
-          case 4:
-            edit();
+          case 4: edit(user);
             break;
-          case 5:
-            destroy();
+          case 5: destroy(user);
             break;
-          case 6:
-            return;
+          case 6: return;
             break;
-          default:
-            cout << get_translated_string("menu_list_admin_user_invalid") << endl;
+          default: cout << get_translated_string("menu_list_admin_user_invalid") << endl;
             break;
         }
       }
